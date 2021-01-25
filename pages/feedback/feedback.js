9f62b3c8deec73f359a92a08d3082971c72f85d8
unknown <chenning@youbanban.com>
2021-01-25 18:23:42 +0800

    2021-01-25

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -39,7 +39,9 @@ Page({
         isActived: false
       }
     ],
-    upImageList: []
+    upImageList: [],
+    textVal: '',
+    loadImageUrlList: []
   },
 
   //点击添加图片
@@ -65,5 +67,75 @@ Page({
     this.setData({
       upImageList
     })
+  },
+
+  //文本域输入事件
+  handleTextarea(e) {
+    const{value} = e.detail
+    this.setData({
+      textVal: value
+    })
+  },
+
+  //点击提交
+  handleSubmit() {
+    //获取文本域的值
+    const {textVal} = this.data;
+     //判断文本域的值是否为空
+    if(!textVal.trim()) {
+      wx.showToast({
+        title: '输入不能为空',
+        icon: 'none',
+        mask: true
+      });
+      return;
+    }
+    
+    wx.showLoading({
+      title: "上传图片中",
+      mask: true
+    });
+    //获取上传图片数组
+    const {upImageList, loadImageUrlList} = this.data;
+    //有选择上传图片
+    if(upImageList.length > 0) {
+    //上传图片到图片服务器
+    upImageList.forEach((item, index) => {
+      wx.uploadFile({
+        url: '',//图片服务器地址
+        filePath: item,
+        name: 'image',
+        formData: {},
+        success: (result)=>{
+          const url = JSON.parse(result.data).url
+          loadImageUrlList.push(url)
+          this.setData({
+            loadImageUrlList
+          })
+          wx.hideLoading();
+          //接口原因，无法上传,打印一下
+          wx.showToast({
+            title: '"已提交到后台"',
+            icon: 'none',
+            mask: true
+          });
+          wx.navigateBack({
+            delta: 1
+          });
+        }
+      });
+    })
+    }else {
+      wx.hideLoading();
+      //没有选择上传图片，只有文字
+      wx.showToast({
+        title: '"已提交到后台"',
+        icon: 'none',
+        mask: true
+      });
+      wx.navigateBack({
+        delta: 1
+      });
+    }
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -49,52 +49,12 @@ Page({
   },
 
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //点击轮播图跳转
+  handleGoodsDetail(e) {
+    let {navigator_url} = e.currentTarget.dataset;
+    let url = navigator_url.replace(/main/,"goods_detail")
+    wx.navigateTo({
+      url
+    });
   }
 })
